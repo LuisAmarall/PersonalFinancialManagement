bbbc015ac9d703dffeec7cd608357223f598f0dc
Luis Amaral <olluisamarall@gmail.com>
2026-06-28 14:42:48 -0300

    Create the domain user entity mapping and prepare configurations to generate the migration.

---
+++ PersonalFinancialManagement.Infrastructure/Persistence/Configuration/UserConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PersonalFinancialManagement.Core.Models.Entities;
+
+namespace PersonalFinancialManagement.Infrastructure.Persistence.Configuration;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.ToTable("Transfers")
+            .HasKey(_ => _.Id);
+
+        builder.OwnsOne(_ => _.FullName, name =>
+        { name.Property(_ => _.IndividualsName).HasColumnName("Name").HasColumnType("VARCHAR").HasMaxLength(100).IsRequired(); });
+
+        builder.OwnsOne(_ => _.EmailAddress, email =>
+        { email.Property(_ => _.EmailAddress).HasColumnName("Email").HasColumnType("VARCHAR").HasMaxLength(80).IsRequired(); });
+
+        builder.OwnsOne(_ => _.Password, password =>
+        { password.Property(_ => _.Key).HasColumnName("Password").HasColumnType("VARCHAR").HasMaxLength(10).IsRequired(); });
+
+        builder.Property(_ => _.CreatedAt).HasColumnName("CreatedAt").HasColumnType("TIMESTAMP").IsRequired();
+
+        builder.Property(_ => _.DeletedAt).HasColumnName("DeletedAt").HasColumnType("TIMESTAMP");
+    }
+}
