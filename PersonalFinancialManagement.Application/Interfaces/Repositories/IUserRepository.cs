8e0d24acf14f2fc0e0863599f268e3a73f6ca6ac
Luis Amaral <olluisamarall@gmail.com>
2026-07-29 19:01:34 -0300

    Creation of the user interface and implementation of the repository to manage database activities.

---
+++ PersonalFinancialManagement.Application/Interfaces/Repositories/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using DomainDesign.ValueObjects;
+using PersonalFinancialManagement.Core.Entities;
+
+namespace PersonalFinancialManagement.Application.Interfaces.Repositories;
+
+public interface IUserRepository
+{
+    Task<User> AddUserAsync(User user);
+    Task<User?> FindUserByIdAsync(Guid id);
+    Task<User?> FindUserByNameAsync(Name name);
+    Task<User?> FindUserByEmailAsync(Email email);
+    Task<User> UpdateUserAsync(User user);
+    Task InactivationUserAsync(User user);
+    Task SaveChangesAsync();
+}
---
+++ PersonalFinancialManagement.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,58 @@
+﻿using DomainDesign.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+using PersonalFinancialManagement.Application.Interfaces.Repositories;
+using PersonalFinancialManagement.Core.Entities;
+using PersonalFinancialManagement.Infrastructure.Persistence.Context;
+
+namespace PersonalFinancialManagement.Infrastructure.Repositories;
+public class UserRepository : IUserRepository
+{
+    readonly ApplicationContext _context;
+
+    public UserRepository(ApplicationContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<User?> FindUserByIdAsync(Guid id)
+    {
+        return await _context.Users.AsNoTracking().FirstOrDefaultAsync(_ => _.Id == id);
+    }
+
+    public async Task<User?> FindUserByEmailAsync(Email email)
+    {
+        return await _context.Users.AsNoTracking().FirstOrDefaultAsync(_ => _.EmailAddress == email);
+    }
+
+    public async Task<User?> FindUserByNameAsync(Name name)
+    {
+        return await _context.Users.AsNoTracking().FirstOrDefaultAsync(_ => _.FullName == name);
+    }
+
+    public async Task<User> AddUserAsync(User user)
+    {
+        await _context.Users.AddAsync(user);
+        return user;
+    }
+
+    public async Task<User> UpdateUserAsync(User user)
+    {
+        var updatedUser = await _context.Users.AsNoTracking().FirstOrDefaultAsync(_ => _.Id == user.Id);
+
+        _context.Entry(updatedUser).CurrentValues.SetValues(user);
+        _context.Update(updatedUser);
+
+        return user;
+    }
+
+    public async Task InactivationUserAsync(User user)
+    {
+        user.Delete();
+        await UpdateUserAsync(user);
+    }
+
+    public Task SaveChangesAsync()
+    {
+        return _context.SaveChangesAsync();
+    }
+}
