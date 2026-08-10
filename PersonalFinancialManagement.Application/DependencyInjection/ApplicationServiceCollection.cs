cfc218f15711e223b344e28e882eb336e9b0f2b2
Luis Amaral <olluisamarall@gmail.com>
2026-08-10 16:29:24 -0300

    Implementation of dependency injection concepts and pre-definition of the dependencies to be used in the application layer.

---
+++ PersonalFinancialManagement.Application/DependencyInjection/ApplicationServiceCollection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace PersonalFinancialManagement.Application.DependencyInjection;
+
+public static class ApplicationServiceCollection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        RegisterServices(services);
+
+        return services;
+    }
+
+    private static void RegisterServices(IServiceCollection services)
+    {
+        //services.AddScoped<ICategoryService, CategoryService>();
+        //services.AddScoped<IToPayService, ToPayService>();
+        //services.AddScoped<IToReceiveService, ToReceiveService>();
+        //services.AddScoped<IUserService, UserService>();
+    }
+}
