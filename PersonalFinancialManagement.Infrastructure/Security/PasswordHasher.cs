f7ca092eb308ee856e12503ddca82599ed06f2a9
Luis Amaral <olluisamarall@gmail.com>
2026-09-21 09:44:33 -0300

    feat (Infrastructure): Implements the application layer abstraction within the infrastructure layer.
    - Adds JWT configurations for the infrastructure.
    - Adds the concrete implementation for database operations.
    - Adds the appropriate configurations to appsettings.json:
    
    The implementations represent the realization of the abstractions for token issuance and management.

---
+++ PersonalFinancialManagement.Infrastructure/Security/PasswordHasher.cs
@@ -0,0 +1,16 @@
+﻿using PersonalFinancialManagement.Application.Interfaces.Security;
+
+namespace PersonalFinancialManagement.Infrastructure.Security;
+
+public sealed class PasswordHasher : IPasswordHasher
+{
+    public string HashPassword(string password)
+    {
+        return BCrypt.Net.BCrypt.HashPassword(password);
+    }
+
+    public bool Verify(string password, string hashedPassword)
+    {
+        return BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+    }
+}
