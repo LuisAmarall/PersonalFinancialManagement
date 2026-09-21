0db1098e5550399a7fadcd6bfd8488908cbf5235
Luis Amaral <olluisamarall@gmail.com>
2026-09-21 04:02:37 -0300

    feat(application): create application layer abstractions
    - Add Error to represent expected system failures.
    - Add IResult and Result to represent success or failure.
    - Add ICommand and ICommandHandler to structure commands.
    - Add IQuery and IQueryHandler to structure queries.
    - Centralize shared application layer contracts.
    
    These abstractions establish the foundation for implementing use cases
    with low coupling between the application and its external layers.

---
+++ PersonalFinancialManagement.Application/Abstractions/IResult.cs
@@ -0,0 +1,7 @@
+﻿namespace PersonalFinancialManagement.Application.Abstractions;
+
+public interface IResult
+{
+    public bool IsSuccess { get; }
+    public IReadOnlyList<Error> Errors { get; }
+}
---
+++ PersonalFinancialManagement.Application/Abstractions/Result.cs
@@ -0,0 +1,29 @@
+﻿
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace PersonalFinancialManagement.Application.Abstractions
+{
+    public sealed class Result : IResult
+    {
+        public bool IsSuccess { get; }
+        public IReadOnlyList<Error> Errors { get; }
+
+        private Result(bool isSuccess, IReadOnlyList<Error> errors)
+        {
+            IsSuccess = isSuccess;
+            Errors = errors;
+        }
+        
+        public static Result Success() => new Result(true, Array.Empty<Error>());
+
+        public static Result Failure(IReadOnlyList<Error> erros)
+        {
+            ArgumentNullException.ThrowIfNull(erros);
+            
+            if (erros.Count == 0)
+                throw new ArgumentException($"A failure must contain at least one error.", nameof(erros));
+
+            return new Result(false, erros.ToList());
+        }
+    }
+}
